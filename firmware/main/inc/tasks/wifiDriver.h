46f57d74a8124be935c6364826c27922aeb1a652
BrunaMS <br.medeiros@hotmail.com>
2022-09-22 21:55:56 -0300

    Create receiver module firmware

---
+++ firmware/main/inc/tasks/wifiDriver.h
@@ -0,0 +1,17 @@
+/*
+    This example shows how to use the All Channel Scan or Fast Scan to connect
+    to a Wi-Fi network.
+
+    In the Fast Scan mode, the scan will stop as soon as the first network matching
+    the SSID is found. In this mode, an application can set threshold for the
+    authentication mode and the Signal strength. Networks that do not meet the
+    threshold requirements will be ignored.
+
+    In the All Channel Scan mode, the scan will end only after all the channels
+    are scanned, and connection will start with the best network. The networks
+    can be sorted based on Authentication Mode or Signal Strength. The priority
+    for the Authentication mode is:  WPA2 > WPA > WEP > Open
+*/
+
+#pragma once
+void wifiService(void* pvParameters);
---
+++ firmware/main/receiver.c
@@ -0,0 +1,13 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "esp_log.h"
+#include "wifiDriver.h"
+
+void app_main(void)
+{
+    TaskHandle_t wifiHandle = NULL;
+
+    xTaskCreate(wifiService, "wifiService", 5000, NULL, configMAX_PRIORITIES - 1, &wifiHandle);
+    vTaskDelay(pdMS_TO_TICKS(5));
+    vTaskDelete(NULL);
+}
